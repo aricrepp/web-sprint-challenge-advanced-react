57cdb9c715d7ccc478f9fbcd0fc315e41399091c
Aric Repp <aricmrepp@gmail.com>
2020-06-12 20:02:36 -0400

    Pushing plantlist, Checkout, useForm, and localStorage

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,12 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useForm = (initialValue, key) => {
+  const [values, setValues] = useLocalStorage(initialValue, key);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return [values, handleChanges];
+};
