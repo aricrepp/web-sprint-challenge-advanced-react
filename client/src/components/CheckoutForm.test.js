85716b31fec6f5262391c5c4343fddd2ca7dfb14
Aric Repp <aricmrepp@gmail.com>
2020-06-12 20:44:29 -0400

    Pushing tests

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,48 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+// import { waitFor,
+//     fireEvent, } from "@testing-library/dom";
+// import '@testing-library/jest-dom/extend-expect'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
 
-test("form shows success message on submit with form details", () => {});
+    const {getByText} = render(<CheckoutForm />);
+
+    const header = getByText("Checkout Form");
+
+    expect(header).toHaveTextContent("Checkout Form");
+
+});
+
+test("form shows success message on submit with form details", () => {
+
+    const {getByTestId} = render(<CheckoutForm />);
+
+    const fName = getByTestId(/firstNameTest/i);
+    const lName = getByTestId(/lastNameTest/i);
+    const address = getByTestId(/address/i);
+    const city = getByTestId(/city/i);
+    const state = getByTestId(/state/i);
+    const zip = getByTestId(/zip/i);
+    const button = getByTestId(/button/i);
+
+    fireEvent.change(fName, {target: {value: 'Aric'}});
+    expect(fName.value).toBe('Aric');
+    fireEvent.change(lName, {target: {value: 'Repp'}});
+    expect(lName.value).toBe('Repp');
+    fireEvent.change(address, {target: {value: '7777 south'}});
+    expect(address.value).toBe('7777 south');
+    fireEvent.change(city, {target: {value: 'nowhere'}});
+    expect(city.value).toBe('nowhere');
+    fireEvent.change(state, {target: {value: 'Boring'}});
+    expect(state.value).toBe('Boring');
+    fireEvent.change(zip, {target: {value: '55555'}});
+    expect(zip.value).toBe('55555');
+    fireEvent.click(button);
+
+    expect(getByTestId(/successMessage/i)).toHaveTextContent("Woo-hoo");
+    
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,8 +1,8 @@
 // write your custom hook here to control your checkout form
 import { useLocalStorage } from "./useLocalStorage";
 
-export const useForm = (initialValue, key) => {
-  const [values, setValues] = useLocalStorage(initialValue, key);
+export const useForm = (key, initialValue) => {
+  const [values, setValues] = useLocalStorage(key, initialValue);
 
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
